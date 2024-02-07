e605f37d092417423ce7dc53920f541900e70c09
fujiN10 <abramenkolev1@yandex.ru>
2024-02-07 19:51:35 +0400

    commit

---
+++ index.py
@@ -0,0 +1,56 @@
+from flask import Flask, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def slash():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+@app.route('/promotion')
+def promotion():
+    a = ['Человечество вырастает из детства.',
+
+'Человечеству мала одна планета.',
+
+'Мы сделаем обитаемыми безжизненные пока планеты.',
+
+'И начнем с Марса!',
+
+'Присоединяйся!']
+    return '</br>'.join(a)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Жди нас, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}" 
+                    <h2>Вот она какая, красная планета.<h2>
+                    <div class="alert alert-primary" role="alert">
+                    </div>
+                  </body>
+                </html>'''
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
